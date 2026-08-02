186140ac680ef69308606e09c21ea4ebb557b360
sistema-financeiro-igreja <jefferson.adm.negocios@gmail.com>
2026-08-02 00:12:40 -0400

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://sxhfgczctilyrlwvweof.supabase.co'
-const supabaseKey = 'sb_publishable_w-tCfTQqWSL6v8Vzp29xUw_04kJfvAL'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
